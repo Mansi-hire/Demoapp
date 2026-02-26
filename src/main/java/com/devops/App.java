c98ee290a1e87a58919503fd65613d62f62808fa
Ubuntu <ubuntu@ip-172-31-10-101.ap-south-1.compute.internal>
2026-02-26 09:49:58 +0000

    Test webhook

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -1,3 +1,4 @@
+
 package com.devops;
 
 /**
@@ -8,6 +9,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!-Webhook test" );
     }
 }
